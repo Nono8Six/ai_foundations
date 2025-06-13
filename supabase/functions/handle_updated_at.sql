3f717c521ce3afa21234f47392fa0ee66220cfd8
Nono8Six <145220678+Nono8Six@users.noreply.github.com>
2025-06-13 12:38:33 +0200

    chore: track missing functions and cleanup script

---
+++ supabase/functions/handle_updated_at.sql
@@ -0,0 +1,9 @@
+-- Function: public.handle_updated_at()
+
+CREATE OR REPLACE FUNCTION handle_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = now();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
