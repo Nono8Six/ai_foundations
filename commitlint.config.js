5677c86cb8d0ccdd17257ae0863036b91b2df7de
Nono8Six <arnaudferron@pm.me>
2025-07-06 18:18:07 +0200

    corr

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,12 @@
 // commitlint.config.js
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Permet les commits sans type (ex: "correction supabase")
+    'type-empty': [0, 'always'],
+    // Désactivé car le parseur échoue sans type, rendant le sujet nul
+    'subject-empty': [0, 'always'],
+    // Assure que le message de commit n'est pas vide (au moins 3 caractères)
+    'header-min-length': [2, 'always', 3],
+  },
 };
