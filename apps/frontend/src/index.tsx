110993550fa77072058a469335027e61c8b79e34
Nono8Six <arnaudferron@pm.me>
2025-07-08 20:41:39 +0200

    correction

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/tailwind.css';
@@ -10,8 +11,10 @@ const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
